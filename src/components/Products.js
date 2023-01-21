2ddb9d0cfab9627f164c6bc198e544101cfafb90
lazarevictina <lazarevictina@gmail.com>
2023-01-21 21:02:59 +0100

    done

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,12 +3,35 @@ import "./Products.css";
 import {useState} from 'react';
 import Card from "./Card";
 const Products = (props) => {
+  const [show,setShow] = useState('All');
+  const onChangeValue =(e) =>{
+    setShow(e.target.value);
+  }
 
   return (
     <div>
-      
+      <div className="filter-container">
+        <div className="filter-title">Filter by Category:</div>
+        <div>
+          <select
+            name="type-list"
+            id="type-list"
+            onChange={onChangeValue}
+          >
+            <option value="All" defaultValue>All</option>
+            <option value="phone">Phone</option>
+            <option value="laptop">Laptop</option>
+            <option value="headphone">Headphone</option>
+          </select>
+        </div>
+      </div>
       <div className="items">
-        {props.productItems.map((productItem) => (
+        {props.productItems.filter(item=>{
+          if(show === item.type || show === 'All'){
+            return true;
+          }
+          return false;
+        }).map((productItem) => (
           <Card key={productItem.id}>
             <div>
               <img
@@ -21,6 +44,7 @@ const Products = (props) => {
               <h3 className="item-name">{productItem.name}</h3>
             </div>
             <div className="item-price">{productItem.price}e</div>
+
             <div>
               <button
                 className="item-add-button"
